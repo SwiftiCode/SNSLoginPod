8f8d01933f33120596b0f85c85b5a28fce3f6fed
SwiftCode <SwiftCode@gamil.com>
2020-07-03 06:50:44 +0800

    new

--- SNSLoginPod/AppDelegate.swift
+++ SNSLoginPod/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 SwiftiCode. All rights reserved.
 //
 
+/*
 import UIKit
 
 @UIApplicationMain
@@ -34,4 +35,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
+*/
 
+//  AppDelegate.swift
+
+import UIKit
+import FBSDKCoreKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+          
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
+
+        return true
+    }
+          
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+
+        ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+        )
+
+    }
+
+}
+
+    
