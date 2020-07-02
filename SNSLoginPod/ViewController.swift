707be5f5dd92d11cc63309cccddb8f22dd91d147
SwiftCode <SwiftCode@gamil.com>
2020-07-02 21:37:00 +0800

    Initial Commit

---
+++ SNSLoginPod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SNSLoginPod
+//
+//  Created by SwiftiCode on 2/7/20.
+//  Copyright © 2020 SwiftiCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
